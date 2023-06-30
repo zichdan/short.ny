8775164410c573230746bac2a8681e27b2e6d1e3
christain chidi okeke <chidiokeken@gmail.com>
2023-06-30 23:30:09 +0100

    removed config dict setings

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from short_ny import create_app
 from .short_ny.config import config_dict
 
-app = create_app(config=config_dict['dev'])
+app = create_app()
 
 if __name__=="__main__":
     app.run(debug=True)
